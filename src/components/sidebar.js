8418aedf4019bb6f4dd71b57d4011b97d3fa230c
parth <admin@test.com>
2023-02-27 03:58:29 +0530

    material ui and icon packs

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../styles/sidebar.css'
+import {Avatar} from '@material-ui/core'
 
 function sidebar() {
   return (
     <div className='sidebar'>
-        <div className="sidebar_header"></div>
+        <div className="sidebar_header">
+            <Avatar />
+        </div>
         <div className="sidebar_search"></div>
         <div className="sidebar_chats"></div>
     </div>
